2b0fe82c999c588ecd1bcae892d29074a4a39061
elkin <elkin.21.74@gmail.com>
2023-03-01 21:12:17 -0500

    -m "prueba 2"

--- ConversorMoneda/src/ClasePrimera.java
+++ ConversorMoneda/src/ClasePrimera.java
@@ -4,6 +4,16 @@ public class ClasePrimera {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
+		
+		String claveDeAceso = "Putavida";
+		
+		if(claveDeAceso == "Putavida") {
+			System.out.println("Lavida es triste");
+			
+		}else {
+			System.out.println("Esto es lo que quieres?");
+		}
 	}
+	
 
 }
